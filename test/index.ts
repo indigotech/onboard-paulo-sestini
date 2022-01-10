dfe4af066a48519cbf88ef741459f34ef077b699
paulo_sestini <24728744+paulosestini@users.noreply.github.com>
2022-01-10 14:47:40 -0300

    Integrated tests to database

--- test/index.ts
+++ test/index.ts
@@ -1,9 +1,16 @@
 import * as request from 'supertest';
 import { startServer } from '../src/setup';
 import { expect } from 'chai';
+import * as dotenv from 'dotenv';
+import { queryCreateUser } from './queries';
+import { User } from '../src/entity/user';
+import * as bcrypt from 'bcrypt';
 
 before(async () => {
+  dotenv.config({ path: __dirname + '/../test.env' });
   await startServer();
+  const userRepository = User.getRepository();
+  userRepository.clear();
 });
 
 describe('Print test', () => {
@@ -19,5 +26,36 @@ describe('Access the server', () => {
     });
 
     expect(response.body.data.hello).to.be.eq('Hello, world!');
+  });
+});
+
+describe('Mutation createUser', () => {
+  it('Run mutation createUser', async () => {
+    const response = await request('localhost:4000').post('/').send(queryCreateUser);
+
+    expect(response.body.data.createUser).to.have.property('id').not.undefined;
+    expect(response.body.data.createUser).to.have.property('name');
+    expect(response.body.data.createUser).to.have.property('email');
+    expect(response.body.data.createUser).to.have.property('birthDate');
+  });
+
+  it('Check user on database', async () => {
+    const userRepository = User.getRepository();
+    const userEmail = queryCreateUser.variables.data.email;
+    const user = await userRepository.findOne({ email: userEmail });
+
+    expect(user).to.not.be.undefined;
+    expect(user).to.have.property('id').not.undefined;
+    expect(user).to.have.property('name');
+    expect(user).to.have.property('email');
+    expect(user).to.have.property('birthDate');
+  });
+
+  it('Check hashed password', async () => {
+    const userRepository = User.getRepository();
+    const user = await userRepository.findOne({ email: queryCreateUser.variables.data.email });
+
+    const plainPassword = queryCreateUser.variables.data.password;
+    expect(await bcrypt.compare(plainPassword, user.password)).to.be.true;
   });
 });
