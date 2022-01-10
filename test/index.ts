4290550a75cb9423f91aa9af419b1bebc4822169
paulo_sestini <24728744+paulosestini@users.noreply.github.com>
2022-01-10 09:36:49 -0300

    Added supertest

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,22 @@
+import * as request from 'supertest';
+import { startServer } from '../src/setup';
+
+before(async () => {
+  await startServer();
+});
+
 describe('Print test', () => {
   it('Printing', () => {
     console.log('Test ran');
+  });
+});
+
+describe('Access the server', () => {
+  it('Query hello', async () => {
+    const response = await request('localhost:4000').post('/').send({
+      query: '{ hello }',
+    });
+
+    console.log(response.text);
   });
 });
